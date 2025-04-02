8db32bc593d3b4165ffac21f38596414649f2eec
John Knotts <jhnnyk@gmail.com>
2025-04-01 18:02:07 -0600

    some basic content and styles

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,24 @@
 <script setup></script>
 
 <template>
-  <main>Badass Todo</main>
+  <main>
+    <h1 class="is-size-1 has-text-centered">Badass Todo</h1>
+
+    <div class="field is-grouped">
+      <p class="control is-expanded">
+        <input type="text" class="input" placeholder="Add a todo" />
+      </p>
+      <p class="control">
+        <a href="#" class="button is-info">Add</a>
+      </p>
+    </div>
+  </main>
 </template>
+
+<style>
+main {
+  max-width: 400px;
+  padding: 20px;
+  margin: 0 auto;
+}
+</style>
