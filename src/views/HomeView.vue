b2df151a7022b854c3a40cf15cbc6ca95a0173d9
John Knotts <jhnnyk@gmail.com>
2025-04-01 19:05:38 -0600

    todo placeholders

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,13 +4,27 @@
   <main>
     <h1 class="is-size-1 has-text-centered">Badass Todo</h1>
 
-    <div class="field is-grouped">
+    <div class="field is-grouped mb-5">
       <p class="control is-expanded">
         <input type="text" class="input" placeholder="Add a todo" />
       </p>
       <p class="control">
         <a href="#" class="button is-info">Add</a>
       </p>
+    </div>
+
+    <div class="card" v-for="i in 3">
+      <div class="card-content">
+        <div class="content">
+          <div class="columns is-mobile is-vcentered">
+            <div class="column">Shave my head</div>
+            <div class="column is-5 has-text-right">
+              <button class="button is-light">&check;</button>
+              <button class="button is-danger ml-2">&cross;</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </main>
 </template>
