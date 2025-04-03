d4ffdb47881ac14cee8030ce02ea41d01be6b15c
John Knotts <jhnnyk@gmail.com>
2025-04-03 15:51:35 -0600

    add todo data

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,12 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+
+const todos = ref([
+  { id: 'id1', content: 'Shave my head', done: false },
+  { id: 'id2', content: 'Take a shower', done: false },
+  { id: 'id3', content: 'Buy milk', done: false },
+])
+</script>
 
 <template>
   <main>
@@ -13,11 +21,11 @@
       </p>
     </div>
 
-    <div class="card" v-for="i in 3">
+    <div class="card" v-for="todo in todos">
       <div class="card-content">
         <div class="content">
           <div class="columns is-mobile is-vcentered">
-            <div class="column">Shave my head</div>
+            <div class="column">{{ todo.content }}</div>
             <div class="column is-5 has-text-right">
               <button class="button is-light">&check;</button>
               <button class="button is-danger ml-2">&cross;</button>
